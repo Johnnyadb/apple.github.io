d663792959dd6d0a04709c756cac7d49ffcbf889
John <John@qq.com>
2024-05-10 08:30:21 +0800

    change ads

--- randomclick.js
+++ randomclick.js
@@ -5,8 +5,9 @@ function init() {
     "https://novel-psychology.com/b_3IVz0.Pt3/pevDb/msVyJ/Z/D/0y1jMoT/kk5WN/D/AowpLtTQUFxUOPT/kR0/MRDZEY",
     "https://novel-psychology.com/bW3nVI0yP.3Dp_vib/mBVWJJZ/Df0/1aMcTXkQ5/NvDHA/4KLXTTU/xtOATikA0rMxDPkC",
     "https://novel-psychology.com/bs3.Vp0rP-3/pKvob/mqVHJOZ/D-0d1NM/z/EH0BOdTIU/5OL/T/UMzjM/TgQK5bNHjIAf",
-    // 'https://shempaurdou.net/4/7436813',
-    'https://www.highcpmgate.com/z8767nw9bd?key=dd73fef32d545b5b5559b3dd496c33b8',
+    'https://shempaurdou.net/4/7436813',
+    'https://www.highcpmgate.com/dmm6uzza8y?key=ae8300ee01b398a137d6f63fcc00bd63',
+    // 'https://www.highcpmgate.com/z8767nw9bd?key=dd73fef32d545b5b5559b3dd496c33b8',
     // "https://mango.mjsj.sbs/",
   ];
  
