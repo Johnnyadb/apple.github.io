726c0d584707bc58cce25a08e9621b78c70d9902
John <John@qq.com>
2024-05-10 12:40:45 +0800

    add ads

--- randomclick.js
+++ randomclick.js
@@ -6,8 +6,13 @@ function init() {
     "https://novel-psychology.com/bW3nVI0yP.3Dp_vib/mBVWJJZ/Df0/1aMcTXkQ5/NvDHA/4KLXTTU/xtOATikA0rMxDPkC",
     "https://novel-psychology.com/bs3.Vp0rP-3/pKvob/mqVHJOZ/D-0d1NM/z/EH0BOdTIU/5OL/T/UMzjM/TgQK5bNHjIAf",
     'https://shempaurdou.net/4/7436813',
+    'https://atservineor.com/4/7420696',
+    'https://glakaits.net/4/7237907',
+    'https://nabauxou.net/4/7420698',
     'https://www.highcpmgate.com/dmm6uzza8y?key=ae8300ee01b398a137d6f63fcc00bd63',
-    // 'https://www.highcpmgate.com/z8767nw9bd?key=dd73fef32d545b5b5559b3dd496c33b8',
+    'https://www.highcpmgate.com/z8767nw9bd?key=dd73fef32d545b5b5559b3dd496c33b8',
+    'https://www.highcpmgate.com/egwhqfd5j4?key=a78fdce973ec5e6fdfe7a340cbe80330',
+    'https://www.highcpmgate.com/e6c1p52s2n?key=6b16c783c5b3542a115fb23958f8a755',
     // "https://mango.mjsj.sbs/",
   ];
  
