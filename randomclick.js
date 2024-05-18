7ad16f73896dd4f28205962d6f1c1296c11bb49e
John <John@qq.com>
2024-05-18 10:06:09 +0800

    add ads

--- randomclick.js
+++ randomclick.js
@@ -8,12 +8,12 @@ function init() {
     "https://altruisticemergency.com/b.3pVt0/Pu3/pYvgb_mvVRJiZeD/0A1oMazRMoznMOT/MY1XLoTSUezyMwzhM_xzM/z/YT",
     "https://altruisticemergency.com/bW3aVR0.Pg3Ip/vLbsmrVAJqZVDA0S1/MHzpMmx/N/jzEdzlLwTnUhzRMBzWEf2VMBTZQA",
 
-    // "https://zuhempih.com/4/7436813",
-    // "https://thoadsaibsou.net/4/7474716",
-    // "https://phuruxoods.com/4/7237907",
-    // "https://bauptost.net/4/7420689",
-    // "https://eptougry.net/4/7420696",
-    // "https://lidsaich.net/4/7420698",
+    "https://zuhempih.com/4/7436813",
+    "https://thoadsaibsou.net/4/7474716",
+    "https://phuruxoods.com/4/7237907",
+    "https://bauptost.net/4/7420689",
+    "https://eptougry.net/4/7420696",
+    "https://lidsaich.net/4/7420698",
 
     'https://www.highcpmgate.com/gqz2615ye8?key=fa0feaae93d41f849f02500024a84fc8',
     'https://www.highcpmgate.com/z8767nw9bd?key=dd73fef32d545b5b5559b3dd496c33b8',
