affe920d4cbfc5ec2661dde185288f37b437924a
John <John@qq.com>
2024-07-18 15:23:28 +0800

    rm Michael ad

--- randomclick.js
+++ randomclick.js
@@ -115,7 +115,7 @@ function init() {
     "https://classic-bonus.com/b.3VVj0NPQ3/pEv/bLmwVXJ/ZjDB0I1lM/zvcAyoM/zHEUw/LdTRUOz_NazAI/zyMRTZEv",
     
     // hil: Michael(w)
-    "https://spry-cat.com/bL3AVB0.PU3/pov/bTmoVSJiZoDJ0N1SNaDrMy1/M_T/gAyXLBTcUc0NMlzWU/xYOdDMMn",
+    // "https://spry-cat.com/bL3AVB0.PU3/pov/bTmoVSJiZoDJ0N1SNaDrMy1/M_T/gAyXLBTcUc0NMlzWU/xYOdDMMn",
 
     // hil: db
     "https://grim-package.com/bO3WV.0sPU3rp/vSbumMVaJkZVD/0U1/N/DDMG2sOOTWcm2zL-TSUR0mMhzbYS5oNYz/cJ",
