ea496bdd6ec892f09beddf1c38ef17a82ebd765e
John <John@qq.com>
2024-07-31 22:05:17 +0800

    update ads

--- randomclick.js
+++ randomclick.js
@@ -3,13 +3,29 @@ function init() {
     // hil: db
     "https://grim-package.com/bO3WV.0sPU3rp/vSbumMVaJkZVD/0U1/N/DDMG2sOOTWcm2zL-TSUR0mMhzbYS5oNYz/cJ",
     "https://www.qrcreate.online",
+    "https://grim-package.com/bO3WV.0sPU3rp/vSbumMVaJkZVD/0U1/N/DDMG2sOOTWcm2zL-TSUR0mMhzbYS5oNYz/cJ",
+    "https://www.qrcreate.online",
+    "https://grim-package.com/bO3WV.0sPU3rp/vSbumMVaJkZVD/0U1/N/DDMG2sOOTWcm2zL-TSUR0mMhzbYS5oNYz/cJ",
+    "https://www.qrcreate.online",
+    "https://grim-package.com/bO3WV.0sPU3rp/vSbumMVaJkZVD/0U1/N/DDMG2sOOTWcm2zL-TSUR0mMhzbYS5oNYz/cJ",
+    "https://www.qrcreate.online",
+    "https://grim-package.com/bO3WV.0sPU3rp/vSbumMVaJkZVD/0U1/N/DDMG2sOOTWcm2zL-TSUR0mMhzbYS5oNYz/cJ",
+    "https://www.qrcreate.online",
+    "https://grim-package.com/bO3WV.0sPU3rp/vSbumMVaJkZVD/0U1/N/DDMG2sOOTWcm2zL-TSUR0mMhzbYS5oNYz/cJ",
+
     // mon: db
-    "https://glooghalty.net/4/7237941",
-    "https://owhopteefi.net/4/7436813",
-    // "https://pseleedrax.net/4/7237942",
-    // "https://beewoupaule.net/4/7420696",
-    // "https://koachewhome.net/4/7237921",
-    // "https://faireegli.net/4/7474716",
+    "https://vorsoutseemt.com/4/7436813",
+    "https://eedootefaug.net/4/7420698",
+    "https://futseerdoa.com/4/7237921",
+    "https://eeboazurg.net/4/7420696",
+    "https://psauwaun.com/4/7614690",
+    "https://ptougeegnep.net/4/7237907",
+
+    "https://nutchaungong.com/4/7420689",
+    "https://maipheeg.com/4/7474716",
+    "https://glocheexi.com/4/7237941",
+    "https://coawoaph.net/4/7237942",
+    "https://laichegloavy.com/4/7631332",
   ];
  
   var didOpen = false;
