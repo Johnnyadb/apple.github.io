d74664b1c4e0814f8c98a29383c74f1eb93c632e
John <John@qq.com>
2024-08-03 11:02:23 +0800

    update ads

--- randomclick.js
+++ randomclick.js
@@ -4,11 +4,8 @@ function init() {
     "https://grim-package.com/bO3WV.0sPU3rp/vSbumMVaJkZVD/0U1/N/DDMG2sOOTWcm2zL-TSUR0mMhzbYS5oNYz/cJ",
     // "https://www.qrcreate.online",
 
-    // hil: db123 
-    "https://classic-bonus.com/bY3.VK0OPK3tpYv/bYmQVDJ-Z/DI0/1/MKzqcYy/MQzzATy/LMTSUGz/NNzTIrzDMXDgMm",
-
-    // hil: Jacklove (let)
-    "https://indolent-ability.com/bF3/Vw0oP.3lpZv/bYm/VgJPZzDI0Q1cNaD-cpzTO/TZYA4EL/TeUl0FNTz/MA5/NxjUkt",
+    // // hil: db123 
+    // "https://classic-bonus.com/bY3.VK0OPK3tpYv/bYmQVDJ-Z/DI0/1/MKzqcYy/MQzzATy/LMTSUGz/NNzTIrzDMXDgMm",
 
     // mon: db
     // "https://nutchaungong.com/4/7420689",
@@ -17,8 +14,8 @@ function init() {
     "https://coawoaph.net/4/7237942",
     "https://laichegloavy.com/4/7631332",
 
-    "https://vorsoutseemt.com/4/7436813",
-    "https://eedootefaug.net/4/7420698",
+    // "https://vorsoutseemt.com/4/7436813",
+    // "https://eedootefaug.net/4/7420698",
     // "https://futseerdoa.com/4/7237921",
     // "https://eeboazurg.net/4/7420696",
     // "https://psauwaun.com/4/7614690",
