f88e852912246dbae72896d25274734aa821c51c
John <John@qq.com>
2024-09-10 14:36:10 +0800

    update ads

--- randomclick.js
+++ randomclick.js
@@ -4,6 +4,9 @@ function init() {
     "https://grim-package.com/b.3yVi0OPz3/pDvqblmoVxJWZ_DC0/1ZNhDOM/2KO/TacQ2HL/TiUO0JMmzLYw5-NizhcK",
     "https://www.qrcreate.online",
 
+    // hil: Brian
+    "https://json.navigateall.com",
+
     // mon: db
     // "https://nutchaungong.com/4/7420689",
     // "https://maipheeg.com/4/7474716",
@@ -15,8 +18,12 @@ function init() {
     "https://eedootefaug.net/4/7420698",
     "https://futseerdoa.com/4/7237921",
     "https://eeboazurg.net/4/7420696",
-    "https://psauwaun.com/4/7614690",
-    "https://ptougeegnep.net/4/7237907",
+    // "https://psauwaun.com/4/7614690",
+    // "https://ptougeegnep.net/4/7237907",
+
+    // juicy
+    "https://sexytalk.mjsj.sbs/tags/",
+
   ];
  
   var didOpen = false;
